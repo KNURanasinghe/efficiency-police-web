94153796ee69d4ec8b7474737f9f08d74edda142
KNURanasinghe <113103000+KNURanasinghe@users.noreply.github.com>
2024-03-13 14:29:54 +0530

    11

--- efficiency_web_app/src/App.js
+++ efficiency_web_app/src/App.js
@@ -7,26 +7,22 @@ import HomePage from './screen/homePage';
 import DepartmentPage from './screen/department';
 import CareerPage from './screen/career';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import SignInPage from './screen/signin'; 
+
 
 function App() {
   return (
     <Router>
       <div className="App">
-      <Switch>
-        {/* <Header />
+        <Header />
         <Switch>
           <Route path="/" exact component={HomePage} />
           <Route path="/department" component={DepartmentPage} />
           <Route path="/career" component={CareerPage} />
         </Switch>
-        <Footer /> */}
-         <Route exact path="/" component={SignInPage} />
-         </Switch>
+        <Footer />
       </div>
     </Router>
   );
 }
 
 export default App;
-
